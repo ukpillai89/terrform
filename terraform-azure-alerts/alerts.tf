223a11c1802c6271ce70409cdbbfb3b8455b2aff
ukpillai89 <ukpillai89@gmail.com>
2021-05-24 11:46:56 +0530

    Update alerts.tf

--- terraform-azure-alerts/alerts.tf
+++ terraform-azure-alerts/alerts.tf
@@ -1,10 +1,16 @@
-module "azurerm_monitor_metric_alert" "my-alert-1" {
-  name                = var.alert_name
-  resource_group_name = var.rg_name
-  scopes              = var.to_scope
-  description         = var.desc
-
+resource "azurerm_monitor_metric_alert" "my-alert-1" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  scopes              = var.scopes
+  description         = var.description
+  dynamic_criteria {
+    metric_namespace = var.metric_namespace
+    metric_name      = var.metric_name
+    aggregation      = var.aggregation
+    operator         = var.operator
+    alert_sensitivity = var.alert_sensitivity
+  }
   action {
-    action_group_id = var.acg
+    action_group_id = var.action_group_id
   }
 }
