9b140a1aa51128db18dd997ba11b85d492e35909
ukpillai89 <ukpillai89@gmail.com>
2021-05-24 11:48:01 +0530

    Create vars.tf

---
+++ terraform-azure-alerts/vars.tf
@@ -0,0 +1,10 @@
+variable "name" {}
+variable "resource_group_name" {}
+variable "scopes" {}
+variable "description" {}
+variable "metric_namespace" {}
+variable "metric_name" {}
+variable "aggregation" {}
+variable "operator" {}
+variable "alert_sensitivity" {}
+variable "action_group_id"{}
