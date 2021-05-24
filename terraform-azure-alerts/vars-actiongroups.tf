19c22d40474d4a9ba2c13d9ac64bca899d4d6aff
ukpillai89 <ukpillai89@gmail.com>
2021-05-24 12:41:17 +0530

    Create vars-actiongroups.tf

---
+++ terraform-azure-alerts/vars-actiongroups.tf
@@ -0,0 +1,8 @@
+variable "name" {}
+variable "resource_group_name" {}
+variable "short_name" {}
+variable "addreser_name" {}
+variable "email_address" {}
+variable "common_alert_schema" {
+  default = true
+}
